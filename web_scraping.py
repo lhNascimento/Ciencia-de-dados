55b129ce3a7287aa02ca797d6573bac13b1a5a7b
lhNascimento <lhn.vov@gmail.com>
2024-05-30 17:29:44 -0300

    #

--- web_scraping.py
+++
@@ -1,3 +0,0 @@
-'''Vamos acessar a seguinte página de notícias do jornal New York Times: https://nyti.ms/3aHRu2D. 
-A partir dessa fonte de informações vamos trabalhar para criar um DataFrame contendo 
-o dia da notícia, o comentário que foi feito, a explicação que foi dada e o link da notícia.'''
